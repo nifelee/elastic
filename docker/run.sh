9d2909a97bf4bd1bbfbdb9e2abeee34dd6ac828b
nifelee <nifelee@gmail.com>
2019-04-24 15:08:49 +0900

    init: 엘라스틱서치 실무 가이드 스터디 환경 설정
    docker 기반 elasticsearch 환경 구축

---
+++ docker/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker run --name elasticsearch \
+  -v /Users/hklee/workspace/elastic/docker/config/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
+  -v /Users/hklee/workspace/elastic/repo:/REPO \
+  -p 9200:9200 \
+  -p 9300:9300 \
+  -e "discovery.type=single-node" \
+  elastic:0.1
