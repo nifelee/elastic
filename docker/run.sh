87dbca3ca8f5ed101f96033a6688e31f06382066
nifelee <nifelee@gmail.com>
2019-04-25 12:04:26 +0900

    http: 3장 토큰 필터 실습 중 추가
    - Ascii Folding 토큰 필터
    - stemmer_eng_filter 토큰 필터
    - synonym_filter 토큰 필터
    - synonym_filter 동의어 사전 추가
      : 동의어 사전 관련 docker-compose 설정 volume 추가

--- docker/run.sh
+++ docker/run.sh
@@ -2,6 +2,7 @@
 
 docker run --name elasticsearch \
   -v /Users/hklee/workspace/elastic/docker/config/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
+  -v /Users/hklee/workspace/elastic/docker/config/analysis:/usr/share/elasticsearch/config/analysis \
   -v /Users/hklee/workspace/elastic/repo:/REPO \
   -p 9200:9200 \
   -p 9300:9300 \
